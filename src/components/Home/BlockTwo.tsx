03f58f5ced1dcf65957654aa803342bdd3116453
edberisha <ed@peak.watch>
2024-10-13 18:00:32 -0400

    changes

--- src/components/Home/BlockTwo.tsx
+++ src/components/Home/BlockTwo.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
-import { Text, Box, Link } from '@chakra-ui/react';
+import { Text, Box, Link, ListItem, List } from '@chakra-ui/react';
 import Image from 'next/image'; // Import from next/image
-import peanutpals from '../assets/images/peanutpals.png';
+// import peanutpals from '../assets/images/peanutpals.png';
 import peakai from '../assets/images/peakai.png';
 import texasholdem from '../assets/images/texasholdem.png';
+import wordapp from '../assets/images/wordapp.png'
 
 const BlockTwo = () => {
-  const [firstopacityPeanut, setFirstOpacityPeanut] = useState(0.7);
-  const [firstopacityPeak, setFirstOpacityPeak] = useState(0.7);
-  const [firstopacityTexas, setFirstOpacityTexas] = useState(0.7);
+  // const [firstopacityPeanut, setFirstOpacityPeanut] = useState(0.7);
+
 
   return (
     <Box
@@ -30,9 +30,131 @@ const BlockTwo = () => {
       <Box 
         gap={20}
         display="flex"
-        flexDir="row"
+        flexDir={["column","column","column","row","row"]}
       >
         <Link
+
+          href="https://wordapp-76cd48b9af07.herokuapp.com/"
+        >
+          <Text
+            color="white"
+            fontFamily="monospace"
+            textAlign="center"
+            fontSize={["13px", "13px", "15px", "15px", "20px", "20px"]}
+          >
+            WORD APP
+          </Text>
+          <Box
+            flex="1"
+            borderRadius="25px" 
+            overflow="hidden" 
+          >
+            <Image 
+              src={wordapp}
+              alt="Word App" // Add an alt text for accessibility
+
+            />
+          </Box>
+          <Text 
+          fontSize={["12px","14px","14px","17px"]}
+          fontFamily="monospace"
+          // border="2px solid yellow"
+          color="white">
+            TECH STACK USED:
+            <p>
+             <List style={{paddingLeft:"20px"}}>
+                <ListItem>Next.js</ListItem>
+                <ListItem>Node.js</ListItem>
+                <ListItem>Express.js</ListItem>
+                <ListItem>ChakraUI</ListItem>
+                <ListItem>PosegreSQL</ListItem>
+                <ListItem>Firebase Authentication</ListItem>
+                <ListItem>AI integration (OpenAI)</ListItem>
+            </List>
+            </p>
+          </Text>
+        </Link>
+        <Link
+
+          href="https://peak-sleek-uyitp8ljz-peak-sentry.vercel.app/"
+        >
+          <Text
+            color="white"
+            fontFamily="monospace"
+            textAlign="center"
+            fontSize={["13px", "13px", "15px", "15px", "20px", "20px"]}
+          >
+            PEAK AI
+          </Text>
+          <Box
+            flex="1"
+            borderRadius="25px" 
+            overflow="hidden" 
+          >
+            <Image 
+              src={peakai}
+              alt="Peak AI"
+
+            />
+
+          </Box>
+          <Text 
+          fontSize={["12px","14px","14px","17px"]}
+          fontFamily="monospace"
+          // border="2px solid yellow"
+          color="white">
+            TECH STACK USED:
+            <p>
+             <List style={{paddingLeft:"20px"}}>
+                <ListItem>Next.js</ListItem>
+                <ListItem>ChakraUI</ListItem>
+                <ListItem>TypeScript</ListItem>
+                <ListItem>React</ListItem>
+                <ListItem>React Spring</ListItem>
+                <ListItem>Three.js</ListItem>
+            </List>
+            </p>
+          </Text>
+        </Link>
+        <Link
+
+          href="https://edberisha.github.io/TexasHoldEm/"
+        >
+          <Text
+            fontFamily="monospace"
+            color="white"
+            textAlign="center"
+            fontSize={["13px", "13px", "15px", "15px", "20px", "20px"]}
+          >
+            TEXAS HOLDEM
+          </Text>
+          <Box
+            flex="1"
+            borderRadius="25px" 
+            overflow="hidden" 
+          >
+            <Image 
+              src={texasholdem}
+              alt="Texas Holdem"
+
+            />
+          </Box>
+          <Text 
+          fontSize={["12px","14px","14px","17px"]}
+          fontFamily="monospace"
+          // border="2px solid yellow"
+          color="white">
+            TECH STACK USED:
+            <p>
+             <List style={{paddingLeft:"20px"}}>
+                <ListItem>React</ListItem>
+                <ListItem>ChakraUI</ListItem>
+                <ListItem>Framer Motion</ListItem>
+            </List>
+            </p>
+          </Text>
+        </Link>
+        {/* <Link
           onMouseOver={() => setFirstOpacityPeanut(1)}
           onMouseLeave={() => setFirstOpacityPeanut(0.5)}
           href="https://edberisha.github.io/PeanutPals/"
@@ -58,62 +180,10 @@ const BlockTwo = () => {
               objectFit="cover" // Ensure the image covers the entire area
             />
           </Box>
-        </Link>
-        <Link
-          onMouseOver={() => setFirstOpacityPeak(1)}
-          onMouseLeave={() => setFirstOpacityPeak(0.5)}
-          href="https://peak-sleek-uyitp8ljz-peak-sentry.vercel.app/"
-        >
-          <Text
-            color="white"
-            fontFamily="monospace"
-            textAlign="center"
-            fontSize={["13px", "13px", "15px", "15px", "20px", "20px"]}
-          >
-            PEAK AI
-          </Text>
-          <Box
-            flex="1"
-            opacity={firstopacityPeak}
-            borderRadius="25px" 
-            overflow="hidden" 
-          >
-            <Image 
-              src={peakai}
-              alt="Peak AI"
-              layout="responsive"
-              objectFit="cover"
-            />
-          </Box>
-        </Link>
-        <Link
-          onMouseOver={() => setFirstOpacityTexas(1)}
-          onMouseLeave={() => setFirstOpacityTexas(0.5)}
-          href="https://edberisha.github.io/TexasHoldEm/"
-        >
-          <Text
-            fontFamily="monospace"
-            color="white"
-            textAlign="center"
-            fontSize={["13px", "13px", "15px", "15px", "20px", "20px"]}
-          >
-            TEXAS HOLDEM
-          </Text>
-          <Box
-            flex="1"
-            opacity={firstopacityTexas}
-            borderRadius="25px" 
-            overflow="hidden" 
-          >
-            <Image 
-              src={texasholdem}
-              alt="Texas Holdem"
-              layout="responsive"
-              objectFit="cover"
-            />
-          </Box>
-        </Link>
+        </Link> */}
+        
       </Box>
+      
     </Box>
   );
 };
